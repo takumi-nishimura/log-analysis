257cd975d0d4d75da0ce781df9122f90a0fb96de
takumi-nishimura <clp13218@nitech.jp>
2022-12-05 14:11:59 +0900

    add log_to_df

--- src/log_analysis.py
+++ src/log_analysis.py
@@ -1,4 +1,6 @@
+import glob
 import json
+
 import pandas as pd
 
 
@@ -29,6 +31,13 @@ class LogAnalysis:
             _df = pd.merge(_df, d, how="outer")
         return _df.sort_values("asctime", ignore_index=True)
 
+    def log_to_df(self, log_files):
+        df_list = []
+        for log_file in log_files:
+            df = self.read_logfile(log_file)
+            df_list.append(df)
+        return self.merge_df(*df_list)
+
 
 if __name__ == "__main__":
     file_path = "./logs/pos_log.log"
@@ -37,6 +46,9 @@ if __name__ == "__main__":
     d2 = LogAnalysis().read_logfile(file_path)
     file_path = "./logs/button_log.log"
     d3 = LogAnalysis().read_logfile(file_path)
-
     d = LogAnalysis().merge_df(d1, d2, d3)
+    print(d)
+
+    data_list = glob.glob("./logs/*.log")
+    d = LogAnalysis().log_to_df(data_list)
     print(d)
